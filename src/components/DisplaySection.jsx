b328061750ada4f3709a385610c4c061f34510ff
Aaron Enyetu <aaronenyetu@gmail.com>
2023-06-18 23:09:35 -0700

    Completed DisplaySection component

--- src/components/DisplaySection.jsx
+++ src/components/DisplaySection.jsx
@@ -4,6 +4,13 @@ export default function DisplaySection()
 {
     return (
         <div className="display-section wrapper">
+            <h2 className="title">New</h2>
+            <p className="text">Brilliant</p>
+            <span className="description">
+                A display that is up to 2x brighter in the sun.
+            </span>
+            <button className="button">Try Me!</button>
+            <button className="back-button">Top</button>
 
         </div>
     )
