0852dcf60bb501d717f6abf3643361b8d1dd25bb
Aaron Enyetu <aaronenyetu@gmail.com>
2023-06-18 22:59:27 -0700

    Completed the SoundSection Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import Nav from "./components/Nav.jsx";
 import Jumbotron from "./components/Jumbotron.jsx";
+import SoundSection from "./components/SoundSection.jsx";
+import DisplaySection from "./components/DisplaySection.jsx";
 
 function App() {
 
   return (
     <div className="App">
       <Nav />
-      <Jumbotron/>
+      <Jumbotron />
+      <SoundSection />
+      <DisplaySection/>
     </div>
   );
 }
---
+++ src/components/DisplaySection.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function DisplaySection()
+{
+    return (
+        <div className="display-section wrapper">
+
+        </div>
+    )
+}
