6b61fcfce1617b3850ba13c3546c7a5be8be21f7
Aaron Enyetu <aaronenyetu@gmail.com>
2023-06-18 22:37:12 -0700

    Jumbotron component complete

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Iphone from "../assets/images/iphone-14.jpg"
+import HoldingIphone from '../assets/images/iphone-hand.png'
 
 
 export default function Jumbotron()
@@ -12,6 +13,16 @@ export default function Jumbotron()
             <span className="description">
                 From $41.62/mo. for 24 mo. or $999 before trade-in
             </span>
+            <ul className="links">
+                <li>
+                    <button className="button">Buy</button>
+                </li>
+                <li>
+                    <a className="link"> Learn More</a>
+                </li>
+
+            </ul>
+            <img className="iphone-img" src={HoldingIphone} alt='iPhone'/>
         </div>
 
     );
